cbc4e77924c0c9788e3567e792e45b95c9dedf24
oliverostojic <oliver.ostojic.swe@gmail.com>
2024-11-19 17:12:23 -0500

    Updated app.py and heroku.yml.

--- src/user_service/app.py
+++ src/user_service/app.py
@@ -24,4 +24,5 @@ def create_app():
 # Add this block to run the app when the script is executed
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=5050)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
